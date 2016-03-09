e7e3171820edf546ff36da586d86bcda1ed45de6
moolamdeepthi <deepthi.moolam@D469.blr.velankani.com>
2016-03-09 16:47:12 +0530

    create project

---
+++ helloworld/src/helloworld/Hello.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		System.out.println("hello world");
+		
+	}
+
+}
